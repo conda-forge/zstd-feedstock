28014900465dac3e6323e94f07be28572e9c79e1
Rolando (Max) Espinoza <me@rmax.io>
2017-10-20 13:34:51 -0300

    Add -lrt to fix undefined reference.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,11 @@ set -ex
 export CFLAGS="${CFLAGS} -O3 -fPIC"
 export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 
+if [ "$(uname)" == "Linux"  ]; then
+  # Fix undefined clock_gettime.
+  export LDFLAGS="${LDFLAGS} -lrt"
+fi
+
 # Build
 make -j$CPU_COUNT
 
