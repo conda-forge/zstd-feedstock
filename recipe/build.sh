d1af459c1c462fe5cac6307109f43e8ee48248d6
Michael Sarahan <msarahan@gmail.com>
2018-04-11 10:17:54 -0500

    cb3-ize

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,28 @@ set -ex
 export CFLAGS="${CFLAGS} -O3 -fPIC"
 export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 
+declare -a _CMAKE_EXTRA_CONFIG
+if [[ ${HOST} =~ .*darwin.* ]]; then
+    if [[ ${_XCODE_BUILD} == yes ]]; then
+        _CMAKE_EXTRA_CONFIG+=(-G'Xcode')
+        _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_ARCHITECTURES=x86_64)
+        _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
+        _VERBOSE=""
+    fi
+    unset MACOSX_DEPLOYMENT_TARGET
+    export MACOSX_DEPLOYMENT_TARGET
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_AR=${AR})
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_RANLIB=${RANLIB})
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_LINKER=${LD})
+fi
+if [[ ${HOST} =~ .*linux.* ]]; then
+    # I hate you so much CMake.
+    LIBPTHREAD=$(find ${PREFIX} -name "libpthread.so")
+    _CMAKE_EXTRA_CONFIG+=(-DPTHREAD_LIBRARY=${LIBPTHREAD})
+    LIBRT=$(find ${PREFIX} -name "librt.so")
+    _CMAKE_EXTRA_CONFIG+=(-DRT_LIBRARIES=${LIBRT})
+fi
+
 if [ "$(uname)" == "Linux"  ]; then
   # Fix undefined clock_gettime.
   export LDFLAGS="${LDFLAGS} -lrt"
