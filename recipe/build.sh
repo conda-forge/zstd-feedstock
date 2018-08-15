1f7f49c158413763a1cb334fca9c328bddbe03e0
Ray Donnelly <mingw.android@gmail.com>
2018-08-15 01:43:02 +0100

    WIP: Fixing cmake build to emit pc files

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,21 @@
 set -ex
 
 export CFLAGS="${CFLAGS} -O3 -fPIC"
-export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 
-declare -a _CMAKE_EXTRA_CONFIG
-if [[ ${HOST} =~ .*darwin.* ]]; then
+# Fix undefined clock_gettime (Is this needed? See above)
+if [[ ${target_platform} =~ linux.* ]]; then
+  export LDFLAGS="${LDFLAGS} -lrt"
+fi
+
+CMAKE_BUILD=yes
+
+if [[ ${CMAKE_BUILD} == yes ]]; then
+
+  declare -a _CMAKE_EXTRA_CONFIG
+
+  if [[ ${HOST} =~ .*darwin.* ]]; then
     if [[ ${_XCODE_BUILD} == yes ]]; then
-        _CMAKE_EXTRA_CONFIG+=(-G'Xcode')
+        CMAKE_GENERATOR="Xcode"
         _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_ARCHITECTURES=x86_64)
         _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
         _VERBOSE=""
@@ -17,22 +26,34 @@ if [[ ${HOST} =~ .*darwin.* ]]; then
     _CMAKE_EXTRA_CONFIG+=(-DCMAKE_AR=${AR})
     _CMAKE_EXTRA_CONFIG+=(-DCMAKE_RANLIB=${RANLIB})
     _CMAKE_EXTRA_CONFIG+=(-DCMAKE_LINKER=${LD})
-fi
-if [[ ${HOST} =~ .*linux.* ]]; then
+  fi
+
+  if [[ ${HOST} =~ .*linux.* ]]; then
     # I hate you so much CMake.
     LIBPTHREAD=$(find ${PREFIX} -name "libpthread.so")
     _CMAKE_EXTRA_CONFIG+=(-DPTHREAD_LIBRARY=${LIBPTHREAD})
     LIBRT=$(find ${PREFIX} -name "librt.so")
     _CMAKE_EXTRA_CONFIG+=(-DRT_LIBRARIES=${LIBRT})
+  fi
+
+  pushd build/cmake
+
+    cmake -G"${CMAKE_GENERATOR}"                  \
+          -DCMAKE_INSTALL_PREFIX="${PREFIX}"      \
+          -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib"  \
+          -DCMAKE_PREFIX_PATH="${PREFIX}"         \
+          "${_CMAKE_EXTRA_CONFIG[@]}"
+
+    make -j${CPU_COUNT}
+    make install PREFIX=${PREFIX}
+
+  popd
+
+else
+
+  make -j${CPU_COUNT}
+  make install PREFIX=${PREFIX}
+
+  exit 1
+
 fi
-
-if [ "$(uname)" == "Linux"  ]; then
-  # Fix undefined clock_gettime.
-  export LDFLAGS="${LDFLAGS} -lrt"
-fi
-
-# Build
-make -j$CPU_COUNT
-
-# Install
-make install PREFIX=$PREFIX
