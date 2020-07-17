c0fc1f771c8d2cdf2cb481eb82924959e974e020
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-17 06:50:05 +0200

    Fix aarch64 build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,8 @@ cmake -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR="lib" ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DZSTD_BUILD_SHARED=ON
 cmake --build . --target install
 copy %PREFIX%\Library\bin\zstd.dll %PREFIX%\Library\bin\libzstd.dll
 copy %PREFIX%\Library\lib\zstd.lib %PREFIX%\Library\lib\libzstd.lib
