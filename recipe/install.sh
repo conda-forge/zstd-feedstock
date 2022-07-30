87e308f1c78f45fd1ad4351fae1fcd1b3f36bce4
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-30 11:38:35 +0100

    try to specify static links at compile time

--- recipe/install.sh
+++ recipe/install.sh
@@ -21,25 +21,22 @@ if [[ ${HOST} =~ .*linux.* ]]; then
   _CMAKE_EXTRA_CONFIG+=(-DRT_LIBRARIES=${LIBRT})
 fi
 
-pushd build/cmake
+if [[ "$PKG_NAME" == *static ]]; then
+  ZSTD_BUILD_STATIC=ON
+else
+  ZSTD_BUILD_STATIC=OFF
+fi
 
+pushd build/cmake
   FULL_AR=`which ${AR}`
   cmake -GNinja                            \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         -DCMAKE_INSTALL_LIBDIR="lib"       \
         -DCMAKE_PREFIX_PATH="${PREFIX}"    \
         -DCMAKE_AR=${FULL_AR}              \
-        -DZSTD_BUILD_STATIC=ON             \
+        -DZSTD_BUILD_STATIC=${ZSTD_BUILD_STATIC} \
         -DZSTD_PROGRAMS_LINK_SHARED=ON     \
         "${_CMAKE_EXTRA_CONFIG[@]}"
 
   ninja install
 popd
-
-if [[ "$PKG_NAME" == *static ]]
-then
-	# relying on conda to dedup package
-	echo "Keeping all files, conda will dedupe"
-else
-	rm -rf ${PREFIX}/lib/*.a
-fi
