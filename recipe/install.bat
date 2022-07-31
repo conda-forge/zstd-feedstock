f6fa361ecd27d9895bbc0ce0b60e0a1c70ffffc0
H. Vetinari <h.vetinari@gmx.com>
2022-08-01 01:17:57 +0200

    clean up bld.bat

--- recipe/install.bat
+++ recipe/install.bat
@@ -1,4 +1,5 @@
 @echo on
+
 pushd "%SRC_DIR%"\build\cmake
 cmake -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -6,14 +7,13 @@ cmake -GNinja ^
     -DCMAKE_INSTALL_LIBDIR="lib" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DZSTD_BUILD_SHARED=ON
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
+
 cmake --build . --target install
-if errorlevel 1 exit 1
-dir
-dir lib
-copy lib\zstd.dll  %PREFIX%\Library\bin\zstd.dll
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
+
+:: duplicate DLL (+ importlib) to also have files with "lib" prefix
 copy %PREFIX%\Library\bin\zstd.dll %PREFIX%\Library\bin\libzstd.dll
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 copy %PREFIX%\Library\lib\zstd.lib %PREFIX%\Library\lib\libzstd.lib
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
