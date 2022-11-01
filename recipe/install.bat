a141c49486db937da55b5f2d987aff4dfe90ef80
Isuru Fernando <isuruf@gmail.com>
2022-11-01 15:47:51 -0500

    zstd-static for windows

--- recipe/install.bat
+++ recipe/install.bat
@@ -1,13 +1,22 @@
 @echo on
 
+if %PKG_NAME% == "zstd-static" (
+  set ZSTD_BUILD_STATIC=ON
+  REM cannot build CLI without shared lib
+  set ZSTD_BUILD_SHARED=ON
+) else (
+  set ZSTD_BUILD_STATIC=OFF
+  set ZSTD_BUILD_SHARED=ON
+)
+
 pushd "%SRC_DIR%"\build\cmake
 cmake -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR="lib" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DZSTD_BUILD_SHARED=ON ^
-    -DZSTD_BUILD_STATIC=OFF ^
+    -DZSTD_BUILD_SHARED=%ZSTD_BUILD_SHARED% ^
+    -DZSTD_BUILD_STATIC=%ZSTD_BUILD_STATIC% ^
     -DZSTD_PROGRAMS_LINK_SHARED=ON ^
     .
 if %ERRORLEVEL% neq 0 exit 1
